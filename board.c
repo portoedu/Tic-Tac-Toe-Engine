819ffa954196f0a78adc4090129e5436a33300c7
portoedu <eduardo.porto@acad.pucrs.br>
2016-06-10 14:56:51 -0300

    #7

--- board.c
+++ board.c
@@ -41,11 +41,11 @@ void update_board(char **board, int hor, int vert, int player)
    'X's and 'O's placed appropriately */
 void print_board(char **board)
 {
- 	  printf("%2c |%2c |%2c \n", board[0][0], board[0][1], board[0][2]);
-      printf("---+---+---\n");
-      printf("%2c |%2c |%2c \n", board[1][0], board[1][1], board[1][2]);
-      printf("---+---+---\n");
-      printf("%2c |%2c |%2c \n", board[2][0], board[2][1], board[2][2]);
+ 	  printf("\t%2c |%2c |%2c \n", board[0][0], board[0][1], board[0][2]);
+      printf("\t---+---+---\n");
+      printf("\t%2c |%2c |%2c \n", board[1][0], board[1][1], board[1][2]);
+      printf("\t---+---+---\n");
+      printf("\t%2c |%2c |%2c \n", board[2][0], board[2][1], board[2][2]);
   putchar ('\n');
   return;
 }
--- main.c
+++ main.c
@@ -11,8 +11,8 @@ void print_usage()
 {
 	system("clear");
 
-    printf("Bem vindo ao Jogo da Velha!\nVocê joga com 'O' e");
-    printf(" o computador (eu) com 'X'\nPara fazer uma jogada, insira");
+    printf("Então isto é um desafio? Eu contra você?!\nVocê joga com 'O' e");
+    printf(" eu jogarei com 'X'\nPara fazer uma jogada, insira");
     printf(" o número da linha que gostaria\nde jogar (1, 2 ou 3");
     printf(" onde 1 é o topo do tabuleiro)");
     printf(" em seguida tecle 'ENTER'\ne então o número da coluna");
@@ -67,55 +67,89 @@ void ai_wins()
 
 }
 
-int main()
-{
-    /* Set up global variables: board and best move */
-    int i;
-    char **board = (char **) malloc (sizeof(char *) * 3);
-    for (i=0; i<3; i++) {
-        board[i] = (char *) malloc (sizeof(char) * 3);
-    }
+void cabecario(){
 
-    /* Begin the game by printing usage instructions and
-       initializing the board */
-    print_usage();
-    init_board(board);
-    /* Play the game */
+system("clear");
+
+printf("\n\n\t\t           ██╗ ██████╗  ██████╗  ██████╗     ██████╗  █████╗     ██╗   ██╗███████╗██╗     ██╗  ██╗ █████╗   \n");
+printf("\t\t           ██║██╔═══██╗██╔════╝ ██╔═══██╗    ██╔══██╗██╔══██╗    ██║   ██║██╔════╝██║     ██║  ██║██╔══██╗  \n");
+printf("\t\t           ██║██║   ██║██║  ███╗██║   ██║    ██║  ██║███████║    ██║   ██║█████╗  ██║     ███████║███████║  \n");
+printf("\t\t      ██   ██║██║   ██║██║   ██║██║   ██║    ██║  ██║██╔══██║    ╚██╗ ██╔╝██╔══╝  ██║     ██╔══██║██╔══██║  \n");
+printf("\t\t      ╚█████╔╝╚██████╔╝╚██████╔╝╚██████╔╝    ██████╔╝██║  ██║     ╚████╔╝ ███████╗███████╗██║  ██║██║  ██║  \n");
+printf("\t\t       ╚════╝  ╚═════╝  ╚═════╝  ╚═════╝     ╚═════╝ ╚═╝  ╚═╝      ╚═══╝  ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝  \n\n\n");
+}
+
+void umjogador(char **board){
+	system("clear");
+	print_usage();
     int hor, vert, alpha;
     move best_move = (move) malloc (sizeof(struct legal_move));
+	 print_board(board);
 
-    print_board(board);
-
-    while (!game_over(board) && (!someone_wins(board, 'X'))) {
+	while (!game_over(board) && (!someone_wins(board, 'X'))) {
 		printf("Digite a linha: ");
         scanf("%i", &hor);
-		printf("Digite a coluna: ");
-		scanf("%i", &vert);
-        hor -= 1; // adjust input values to match array indeces
-        vert -= 1;
-        if ((hor > 2) || (vert > 2) || (hor < 0) || (vert < 0)){
-            
-            printf("\nOps, este lugar não existe.\n");
-        } else if (board[hor][vert] != '-') {
-            printf("\nOpa, este lugar já está ocupado!\n");
-        } else {
-            system("clear");
-            update_board(board, hor, vert, 0);
-            printf("A posição depois de sua última jogada:\n");
-            print_board(board);
-            alpha = minimax(board, 1, best_move);
-            update_board(board, best_move -> hor, best_move -> vert, 1);
-            printf("A posição depois da última jogada do computador:\n");
-            print_board(board);
-        }
+		hor -= 1;
+		if(hor < 0 || hor > 2){
+			printf("\nLinha inserida é invalida!\n");}
+		else {
+			printf("Digite a coluna: ");
+			scanf("%i", &vert);
+		    vert -= 1;
+		    if ((vert > 2) || (vert < 0)){
+		        printf("Ops, esta coluna é inválida.\n");
+		    } else if (board[hor][vert] != '-') {
+		        printf("Opa, este lugar já está ocupado!\n");
+		    } else {
+		        system("clear");
+		        update_board(board, hor, vert, 0);
+		        printf("A posição depois de sua última jogada:\n");
+		        print_board(board);
+		        alpha = minimax(board, 1, best_move);
+		        update_board(board, best_move -> hor, best_move -> vert, 1);
+		        printf("A posição depois da última jogada do computador:\n");
+		        print_board(board);
+		    }
+		} 
     }
-
-
-    if (someone_wins(board, 'X')) {
+	if (someone_wins(board, 'X')) {
         ai_wins();
     } else {
         cats_game();
     }
+}
+
+
+int main()
+{
+    /* Set up global variables: board and best move */
+    int i, caso;
+    char **board = (char **) malloc (sizeof(char *) * 3);
+    for (i=0; i<3; i++) {
+        board[i] = (char *) malloc (sizeof(char) * 3);
+    }
+	
+	cabecario();
+    /* Begin the game by printing usage instructions and
+       initializing the board */
+    
+    init_board(board);
+    /* Play the game */
+
+	printf("\t\t\t\t\t\t Escolha uma opção de jogo: \n");
+	printf("\t\t\t\t\t\t |1- Um jogador\n");
+	printf("\t\t\t\t\t\t |2- Dois jogadores\n");
+	scanf("%d", &caso);
+	switch(caso){
+    case 1:
+		  	umjogador(board);
+			break;
+	case 2:
+			
+			break;
+	default: printf("Inválido"); 
+	}
+
 
     return 0; // control should never reach here
 }
